5583399d3e4b5ad76441ada5772bcfab5217ad53
RAHUL Yadav <68323930+RahulYadav242@users.noreply.github.com>
2025-07-06 14:16:35 +0530

    Update utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,17 +1,18 @@
 import jwt from "jsonwebtoken";
 
+
+
 export const generateToken = (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
   res.cookie("jwt", token, {
-  maxAge: 7 * 24 * 60 * 60 * 1000,
-  httpOnly: true,
-  sameSite: "lax",
-  secure: false, // ✅ hardcoded to false for localhost
-});
-
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",     // ✅ HTTPS only
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✅ allow cross-site
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
   return token;
 };
